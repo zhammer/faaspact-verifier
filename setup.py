bb8e74361b9209b463b3f63a78eabc23d8ee1b37
Zach Hammer <zachary_hammer@alumni.brown.edu>
2018-12-05 17:36:53 -0500

    0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='faaspact-verifier',
-    version='0.0.1',
+    version='0.0.2',
     description='Verify pacts for python faas microservices.',
     url='https://github.com/zhammer/faaspact-verifier',
     packages=find_packages(),
